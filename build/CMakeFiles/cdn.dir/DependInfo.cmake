a4b0e59abdf60f9453e4b0671edff2d0177d2af5
lhb-asura <2370205507@qq.com>
2017-03-26 15:16:39 +0800

    first commit

---
+++ build/CMakeFiles/cdn.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/cdn/cdn.cpp" "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/build/CMakeFiles/cdn.dir/cdn.cpp.o"
+  "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/cdn/deploy.cpp" "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/build/CMakeFiles/cdn.dir/deploy.cpp.o"
+  "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/cdn/io.cpp" "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/build/CMakeFiles/cdn.dir/io.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/cdn/lib"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/cdn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cdn.dir/io.cpp.o"
+  "CMakeFiles/cdn.dir/deploy.cpp.o"
+  "CMakeFiles/cdn.dir/cdn.cpp.o"
+  "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/bin/cdn.pdb"
+  "/home/cust/Downloads/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question/HUAWEI_Code_Craft_2017_Preliminary_Contest_Question_zh_v1.3/SDK-gcc/bin/cdn"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cdn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
